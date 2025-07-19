5e242089647d1291b5920ae364d6a186d9476c7e
RXS0NXNT <ratchakrit.w@ku.th>
2025-07-19 17:40:55 +0700

    Add files via upload

--- IntegerSet.java
+++ IntegerSet.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
 /** ADT
- * 
+ * เป็นที่เก็บ integer ที่เรียงลำดับจากน้อยไปมากเเละไม่เป็นค่า null เเละตัวเลขไม่ซ้ำกัน
  */
 public class IntegerSet {
-    ArrayList<Integer> Number; // rep
-
+    ArrayList<Integer> Number; 
+    /** Abstraction Function (AF):
+     * -AF(Number) เซตของตัวเลขทั้งหมดที่อยู่ใน Integer Number 
+     * 
+     * Rep Invariant (RI):
+     * -ตัวเลขใน Number จะต้องไม่เป็น null 
+     * -ตัวเลขใน Number นั้นจะต้องไม่เป็นตัวเลขที่ซ้ำกัน
+     * -ตัวเลขใน Number จะต้องเรียงลำดับจากน้อยไปมาก
+    */
     public IntegerSet(){
         Number = new ArrayList<>();
         CheckRep();
     }
+    /**
+     * ตรวจสอบว่า Rep Invariant เป็นจริงหรือไม่
+     * ตรวจสอบว่าตัวเลขจะไม่ซ้ำกัน และต้องเรียงจากน้อยไปมาก
+     */
     private void CheckRep(){
          if (Number == null) {
                 throw new RuntimeException("Rep invariant violated");
@@ -23,20 +34,48 @@ public class IntegerSet {
         }
     }
     /**
-     * 
-     * 
+     * เช็คก่อนว่าไม่เป็น null
+     * เพิ่มตัวเลขเข้าเซต
+     * หาจุดที่จะไปอยู่เพื่อเรียงจากน้อยไปมาก
+     * @param x ตัวเลขที่ต้องการเพิ่มเข้า
      */
     public void add(Integer x){
-        Number.add(x);
-        CheckRep();
+        if (x == null) {
+            throw new IllegalArgumentException("Cannot add null");
     }
+        if (!contains(x)) {
+            int i = 0;
+        while (i < Number.size() && Number.get(i) < x) {
+            i++;
+        }
+            Number.add(i, x);
+            CheckRep();
+        }
+    }
+    /**
+     * เช็คก่อนว่าไม่เป็น null
+     * แล้วค่อยลบตัวเลขออกจากเซต
+     * @param x ตัวเลขที่ต้องการลบออก
+     */
     public void remove(Integer x){
-        Number.remove(x);
-        CheckRep();
+        if (x == null) {
+            throw new IllegalArgumentException("Cannot remove null");
     }
+        if (contains(x)) {
+            Number.remove(x);
+            CheckRep();
+        }
+    }
+    /**
+     * คำสั่งที่ไว้เเปลงจากตัวเลขไปเป็นสตริง
+     */
     public String toString(){
         return Number.toString();
     }
+    /**
+     * เช็คตัวเลขว่ามีอยู่จริงหรือไม่
+     * @param x ตัวเลขที่เอามาเช็คว่าตัวเลขนี้มีอยู่ในเซตหรือไม่
+     */
     public boolean contains(Integer x){
         for (int i = 0; i < Number.size(); i++) {
             if (x == Number.get(i)) {
@@ -44,8 +83,5 @@ public class IntegerSet {
             }
         }
         return false;
-    }
-    
-    
+    } 
 }
-   
--- IntegerSetManualTest.java
+++ IntegerSetManualTest.java
@@ -2,40 +2,43 @@
 public class IntegerSetManualTest {
     public static void main(String[] args) {
         IntegerSet a = new IntegerSet();
-    
+        //เพิ่มเลข 50 เข้าเซต
         a.add(50);
         String test = "[50]";   
         if (a.toString().equals(test)) {
-            System.out.println("Pass");
+            System.out.println("Test1 Pass ");
         } else {
-            System.out.println("Fail");
+            System.out.println("Fail can't add this number");
         }
+        //เพิ่มเลข 70 เข้าเซต
         a.add(70);
         test = "[50, 70]";   
         if (a.toString().equals(test)) {
-            System.out.println("Pass");
+            System.out.println("Test2 Pass");
         } else {
-            System.out.println("Fail");
+            System.out.println("Fail can't add this number");
         }
+        //เพิ่มเลข 80 เข้าเซต
         a.add(80);
         test = "[50, 70, 80]";  
         if (a.toString().equals(test)) {
-            System.out.println("Pass");
+            System.out.println("Test3 Pass");
         } else {
-            System.out.println("Fail");
+            System.out.println("Fail can't add this number");
         }
+        //ลบเลข 80 ออกจากเซต
         a.remove(80);
         test = "[50, 70]";  
         if (a.toString().equals(test)) {
-            System.out.println("Pass");
+            System.out.println("Test4 Pass");
         } else {
-            System.out.println("Fail don't have this to remove");
+            System.out.println("Fail don't have this number to remove");
         }
-        
+        //เช็คว่ามีเลข 70 อยู่ในเซต
         if (a.contains(70)) {
-            System.out.println("Pass");
+            System.out.println("Test5 Pass");
         } else {
-            System.out.println("Fail");
+            System.out.println("Fail don't have this number in set");
         }
         
         
